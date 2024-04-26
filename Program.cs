48b6ad26d89608594a0ec586db0ab4be496ee6b1
tshepangkagiso <tshepangkagisomashigo8@gmailcom>
2024-04-26 09:27:40 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿using ATM_APP;
+
+// Get user Details
+Console.Write("Please enter your Full Names:");
+string fullnames = Console.ReadLine();
+Console.Write("Please create a new pin code:");
+int pincode = Convert.ToInt32(Console.ReadLine());
+Console.Write("Please enter the value you want to deposit into your account:");
+double accountvalue = Convert.ToDouble(Console.ReadLine());
+
+// Create new User
+UserRegistration Client1 = new UserRegistration( fullnames, pincode, accountvalue);
+
+
+// Display user details
+Console.WriteLine($"Your full name is: {Client1._FullName}");
+AddSpace();
+Console.WriteLine($"Your pincode is: {Client1._Pincode}");
+AddSpace();
+Console.WriteLine($"Your account value is: {Client1._AccountValue}");
+
+//populating users
+UserRegistration Client2 = new UserRegistration("John Blake", 2468, 3000);
+UserRegistration Client3 = new UserRegistration("Blake John", 3692, 10000);
+UserRegistration Client4 = new UserRegistration("Cyril Ramaphosa", 1234, 65841);
+UserRegistration Client5 = new UserRegistration("Leyora Nyora", 1122, 54651);
+UserRegistration Client6 = new UserRegistration("Baby Girl", 3344, 1500);
+UserRegistration Client7 = new UserRegistration("Bra Yaka", 1458, 45654);
+
+List<UserRegistration> list1 = new List<UserRegistration>
+{
+    Client1, Client2, Client3, Client4, Client5, Client6, Client7
+};
+
+
+//Login
+AddSpace();
+
+Console.Write("Enter your Full Name:");
+string nameSearch = Console.ReadLine();
+Console.Write("Enter your Pin");
+int pincodeTry = Convert.ToInt32(Console.ReadLine());
+bool login = false;
+
+
+foreach (UserRegistration item in list1)
+{
+    if(item._FullName == nameSearch)
+    {
+        if(item._Pincode == pincodeTry)
+        {
+            Console.WriteLine("Successful Login");
+            login = true;
+        }
+        else
+        {
+            Console.WriteLine("Incorrect Pincode");
+            login = false; 
+        }
+    }
+    else
+    {
+        Console.WriteLine("That name is not found");
+        login = false;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+Console.ReadLine();
+void AddSpace()
+{
+    Console.WriteLine("");
+    Console.WriteLine("");
+}
+
+
---
+++ UserRegistration.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM_APP
+{
+    public class UserRegistration
+    {
+        //property fields
+        private string FullName = "User Full Names";
+        private int Pincode;
+        private double AccountValue;
+
+        //constructor
+        public UserRegistration(string _FullName, int _Pincode, double _AccountValue)
+        {
+            FullName= _FullName;
+            Pincode= _Pincode;
+            AccountValue= _AccountValue;
+        }
+
+        public string _FullName
+        {
+            get { return FullName; }
+            set { FullName = value; }
+        
+        }
+
+        public int _Pincode
+        {
+            get { return Pincode; }
+            set
+            {
+                if (value == 0)
+                {
+                    Pincode = 0000;
+                }
+                else
+                {
+                    Pincode = value;
+                }
+            }
+        }
+
+        public double _AccountValue
+        {
+            get { return AccountValue; }
+            set
+            {
+                if (value < 100)
+                {
+                    AccountValue = 0;
+                }
+                else
+                {
+                    AccountValue = value;
+                }
+            }
+        }
+
+
+        
+    }
+
+}
